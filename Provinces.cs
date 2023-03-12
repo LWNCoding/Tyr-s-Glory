c38b6e982e9926a5349af12caa0f023ff74e3166
noneal14 <noneal@gmu.edu>
2023-03-12 17:48:32 -0400

    Typo corrected and adjacency list

--- Provinces.cs
+++ Provinces.cs
@@ -11,6 +11,8 @@ public class Province : Node2D
 	*/
 	private int numUnits;
 
+	private List<Province> adjacency;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
