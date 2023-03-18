f05ed0a9a0942d13515e05fd1acc41df17eaae7e
Aaryan Paudyal <aaryanpaudyal@gmail.com>
2023-03-18 14:05:36 -0400

    Added documentation and changed the constructor to include region. Also modified the current player member to create a deep copy each time.

--- Province.cs
+++ Province.cs
@@ -21,49 +21,76 @@ public class Province : Node2D
     public Province()
     {
         this.label = "";
+        this.region = "";
         this.adjacency = new LinkedList<Province>();
         this.visited = false;
         this.currentPlayer = null;
     }
 
-    /// <summary>
+	/// <summary>
     /// Constructor for the Province defined type.
     /// </summary>
-    /// <param name="label">The unique identifier which specifies the name of a province.</param>
-    /// <exception cref="ArgumentException">Thrown when the playerId parameter is less than 0.</exception>
-    public Province(string label,int playerId){
+    /// <param name="label">Parameter for the label of the province.</param>
+    /// <param name="region">Parameter for the region this province is grouped under.</param>
+    /// <param name="playerId">Parameter for the playerId of the player that currently owns this province.</param>
+    public Province(string label,string region,int playerId){
 		this.label = label;
-		this.adjacency = new LinkedList<Province>();
+        this.region = region;
+        this.adjacency = new LinkedList<Province>();
 		this.visited = false;
-		this.currentPlayer = null;
+		this.currentPlayer = new Player(playerId);
     }
 
+	/// <summary>
+    /// Accessor for the region member.
+    /// </summary>
+    /// <returns>The string region.</returns>
 	public string getRegion()
 	{
 		return this.region;
 	}
 
-    public void setRegion(string newRegion) {
-        this.region = newRegion;
-    }
-
+	/// <summary>
+    /// Accessor for the player member.
+    /// </summary>
+    /// <returns>The current player at this province.</returns>
     public Player getPlayer()
     {
     	return this.currentPlayer;
     }
 
-    public void setPlayer(string newPlayer) {
-    	this.currentPlayer = newPlayer;
+	/// <summary>
+    /// Mutator for the player member.
+    /// </summary>
+    /// <param name="newPlayerId">The player id for the new player that owns this current province.</param>
+    /// <exception cref="ArgumentException">Thrown when the newPlayerId parameter is less than 0.</exception>
+    public void setPlayer(int newPlayerId) {
+        if (newPlayerId < 0) {
+            throw new ArgumentException("Construction aborted: The player id must be greater than or equal to 0");
+        }
+        this.currentPlayer = new Player(newPlayerId);
     }
 
+	/// <summary>
+    /// Accessor for the label member.
+    /// </summary>
+    /// <returns>The string label.</returns>
     public string getLabel() {
         return this.label;
     }
 
+	/// <summary>
+    /// Accessor for the visited member.
+    /// </summary>
+    /// <returns>The boolean visited</returns>
     public bool getVisited() {
         return this.visited;
     }
 
+	/// <summary>
+    /// Mutator for the visited member.
+    /// </summary>
+    /// <param name="hasVisited">The new boolean value for the visited member.</param>
     public void setVisited(bool hasVisited) {
         this.visited = hasVisited;
     }
